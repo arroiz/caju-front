0189d37fdcca6aab6ac7529fd535fcc108d0a3a7
Marcos Taron <marcostaron@Marcoss-MacBook-Pro.local>
2024-11-08 17:30:28 -0300

    fix: dependencies and eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from 'react-dom/client';
+
 import App from "./App";
 import './index.css'
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+
+const container = document.getElementById('app');
+const root = createRoot(container!);
+root.render(<App />);
