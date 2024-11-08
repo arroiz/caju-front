f4843055290fec328ac172d231159b997c3a3934
Marcos Taron <marcostaron@Marcoss-MacBook-Pro.local>
2024-11-08 17:57:14 -0300

    chore: configure prettier and editor config

--- src/pages/Dashboard/components/Columns/index.tsx
+++ src/pages/Dashboard/components/Columns/index.tsx
@@ -1,11 +1,10 @@
-
-import * as S from "./styles";
-import RegistrationCard from "../RegistrationCard";
+import * as S from './styles';
+import RegistrationCard from '../RegistrationCard';
 
 const allColumns = [
-  { status: 'REVIEW', title: "Pronto para revisar" },
-  { status: 'APPROVED', title: "Aprovado" },
-  { status: 'REPROVED', title: "Reprovado" },
+  { status: 'REVIEW', title: 'Pronto para revisar' },
+  { status: 'APPROVED', title: 'Aprovado' },
+  { status: 'REPROVED', title: 'Reprovado' },
 ];
 
 type Props = {
@@ -18,17 +17,10 @@ const Collumns = (props: Props) => {
         return (
           <S.Column status={collum.status} key={collum.title}>
             <>
-              <S.TitleColumn status={collum.status}>
-                {collum.title}
-              </S.TitleColumn>
+              <S.TitleColumn status={collum.status}>{collum.title}</S.TitleColumn>
               <S.CollumContent>
                 {props?.registrations?.map((registration) => {
-                  return (
-                    <RegistrationCard
-                      data={registration}
-                      key={registration.id}
-                    />
-                  );
+                  return <RegistrationCard data={registration} key={registration.id} />;
                 })}
               </S.CollumContent>
             </>
--- src/pages/Dashboard/components/RegistrationCard/index.tsx
+++ src/pages/Dashboard/components/RegistrationCard/index.tsx
@@ -1,11 +1,6 @@
-import { ButtonSmall } from "~/components/Buttons";
-import * as S from "./styles";
-import {
-  HiOutlineMail,
-  HiOutlineUser,
-  HiOutlineCalendar,
-  HiOutlineTrash,
-} from "react-icons/hi";
+import { ButtonSmall } from '~/components/Buttons';
+import * as S from './styles';
+import { HiOutlineMail, HiOutlineUser, HiOutlineCalendar, HiOutlineTrash } from 'react-icons/hi';
 
 type Props = {
   data: any;
@@ -27,7 +22,7 @@ const RegistrationCard = (props: Props) => {
         <span>{props.data.admissionDate}</span>
       </S.IconAndText>
       <S.Actions>
-        <ButtonSmall bgcolor="rgb(255, 145, 154)" >Reprovar</ButtonSmall>
+        <ButtonSmall bgcolor="rgb(255, 145, 154)">Reprovar</ButtonSmall>
         <ButtonSmall bgcolor="rgb(155, 229, 155)">Aprovar</ButtonSmall>
         <ButtonSmall bgcolor="#ff8858">Revisar novamente</ButtonSmall>
 
