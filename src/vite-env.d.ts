4cbfadbf466a2870ca40e1d3ad8a3ca15bd5ea09
Marcos Taron <marcostaron@Marcoss-MacBook-Pro.local>
2024-11-10 23:47:53 -0300

    feat: extend tanstack query types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,3 +7,16 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+import '@tanstack/react-query';
+
+interface MyMeta extends Record<string, unknown> {
+  errorMessage: string;
+}
+
+declare module '@tanstack/react-query' {
+  interface Register {
+    queryMeta: MyMeta;
+    mutationMeta: MyMeta;
+  }
+}
